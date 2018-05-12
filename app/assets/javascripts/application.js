1e2fe73a14df5142fec4b69465cfba1b72f366be
kazoo <kazuki_saito@diveintocode.jp>
2018-05-12 11:45:11 +0900

    fullcalendar実装1.1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,29 @@
 
 $(document).on('turbolinks:load', function() {
   console.log("It works on each visit!");
-	$('#calendar').fullCalendar();
+ 
+$('#calendar').fullCalendar({
+//カレンダーのデフォルト表示とテーマを設定
+  defaultView: 'agendaWeek',
+  themeSystem: 'bootstrap4',
+  header: {
+  left: 'prev,next today',
+  center: 'title',
+  right: 'month,agendaWeek,agendaDay,listMonth'
+  },
+
+  dayClick: function(date, jsEvent, view) {
+    alert('Click on : ' + date.format());
+    alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+    alert('Current view: ' + view.name);
+
+    $(this).css('background-color', 'blue');
+  },
+
+  weekNumbers: true,
+  eventLimit: true, // allow "more" link when too many events
+  events: 'https://fullcalendar.io/demo-events.json'
 });
+
+});
+
