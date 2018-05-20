9497b6852e075d39ba011e2a53d196ef66c63641
kazoo <kazuki_saito@diveintocode.jp>
2018-05-20 18:55:18 +0900

    registrationのcontroller修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def create 
+    @user = User.new(sign_up_params)
+	@user.skip_cpnfirmation!
+	@user.save
+ 
+  end
   # GET /resource/sign_up
   # def new
   #   super
