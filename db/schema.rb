36c89fe2fca668b942a895e87964bc37d59935d1
kazoo <kazuki_saito@diveintocode.jp>
2018-05-21 13:44:35 +0900

    migrationFile修正テスト1.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519061855) do
+ActiveRecord::Schema.define(version: 20180521044343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
-    t.text "description"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.text "description"
   end
 
   create_table "events", force: :cascade do |t|
